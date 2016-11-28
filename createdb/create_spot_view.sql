7cf4a8e96e004a7385084b22474cde72c68330e2
vvanhees <vincentvanhees@gmail.com>
2016-11-28 15:20:29 +0100

    bugs fixed in create_spot_view

--- createdb/create_spot_view.sql
+++ createdb/create_spot_view.sql
@@ -5,11 +5,11 @@ select
   da.name as dataset_name,
   email.subject, email.sender, email.receiver, email.cc, email.bcc, send_time,
   topic.name as topicname, topic.id as topic_id, lda.id as lda_id,
-  email_blob.email_id as emailid,
-  email_blob.topic_probability, distance.distance,
-  distance.topic_id1 as dist_topic1, distance.topic_id2 as dist_topic2,
-  dict_word as word,
-  topic_words.probability as word_probability
+  emb.email_id as emailid,
+  emb.topic_probability, dis.distance,
+  dis.topic_id1 as dist_topic1, dis.topic_id2 as dist_topic2,
+  dic.word as word,
+  towo.probability as word_probability
 FROM lda_settings ldas JOIN lda ON (ldas.id = lda.lda_settings_id)
             JOIN dataset da ON (da.id = lda.dataset_id)
             JOIN email  ON (da.id = email.dataset_id)
@@ -17,4 +17,4 @@ FROM lda_settings ldas JOIN lda ON (ldas.id = lda.lda_settings_id)
             JOIN distance dis ON (dis.lda_id = lda.id)
             JOIN email_blob emb ON (emb.email_id = email.id)
             JOIN dict dic ON (dic.lda_id = topic.id)
-            JOIN topic_words towo ON (towo.topic_id = topic.id, dic.id = towo.word_id)
+            JOIN topic_words towo ON (towo.topic_id = topic.id)
